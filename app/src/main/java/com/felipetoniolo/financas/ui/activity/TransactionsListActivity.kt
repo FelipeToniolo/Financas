7bc277ba3b057ad7d8330e5f61f684b49b827c52
Felipe <ftoniolo.aat@gmail.com>
2019-02-19 23:16:20 -0300

    Iniciando App Finanças

---
+++ app/src/main/java/com/felipetoniolo/financas/ui/activity/TransactionsListActivity.kt
@@ -0,0 +1,22 @@
+package com.felipetoniolo.financas.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import com.felipetoniolo.financas.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+class TransactionsListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val transacoes = listOf("Comida - R$20,50", "Economia - R$100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
+
+    }
+}
